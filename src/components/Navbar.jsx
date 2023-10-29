a8295a30f5fb7ad7be8307104ca43a3b0ba6954c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-29 14:20:57 +0600

    created the navbar structure and designed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
+
 export default function Navbar() {
-    return (
-        <div>
-            navbar
-        </div>
-    );
+	return (
+		<Stack
+			direction='row'
+			alignItems='center'
+			// justifyContent='space-between'
+			p={2}
+			sx={{
+				background: '#000',
+				position: 'sticky',
+				top: 0,
+				justifyContent: 'space-between',
+			}}>
+			<Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+				<img src={logo} alt='logo' height={45} />
+			</Link>
+			<SearchBar />
+		</Stack>
+	);
 }
